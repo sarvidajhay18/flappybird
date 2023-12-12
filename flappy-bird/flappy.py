d0e63f34c5105d2c9c0b0150c4265a11c9ac0def
sarvidajhay18 <152369244+sarvidajhay18@users.noreply.github.com>
2023-12-12 05:00:23 -0800

    Update flappy.py

--- flappy-bird/flappy.py
+++ flappy-bird/flappy.py
@@ -1,170 +1,191 @@
+# Importing the libraries
 import pygame
-from pygame.locals import *
 import sys
 import time
 import random
 
-# 950 x 500    
-    
-class Game:
-   
-    def __init__(self):
-        self.w=950
-        self.h=500
-        self.reset=True
-        self.active = False
-        self.input_text=''
-        self.word = ''
-        self.time_start = 0
-        self.total_time = 0
-        self.accuracy = '0%'
-        self.results = 'Time:0 Accuracy:0 % Wpm:0 '
-        self.wpm = 0
-        self.end = False
-        self.HEAD_C = (255,213,102)
-        self.TEXT_C = (240,240,240)
-        self.RESULT_C = (255,70,70)
-        
-       
-        pygame.init()
-        self.open_img = pygame.image.load('type-speed-open.png')
-        self.open_img = pygame.transform.scale(self.open_img, (self.w,self.h))
+# Initializing the pygame
+pygame.init()
+
+# Frames per second
+clock = pygame.time.Clock()
 
 
-        self.bg = pygame.image.load('background.jpg')
-        self.bg = pygame.transform.scale(self.bg, (500,750))
-
-        self.screen = pygame.display.set_mode((self.w,self.h))
-        pygame.display.set_caption('Type Speed test')
-       
-        
-    def draw_text(self, screen, msg, y ,fsize, color):
-        font = pygame.font.Font(None, fsize)
-        text = font.render(msg, 1,color)
-        text_rect = text.get_rect(center=(self.w/2, y))
-        screen.blit(text, text_rect)
-        pygame.display.update()   
-        
-    def get_sentence(self):
-        f = open('sentences.txt').read()
-        sentences = f.split('\n')
-        sentence = random.choice(sentences)
-        return sentence
-
-    def show_results(self, screen):
-        if(not self.end):
-            #Calculate time
-            self.total_time = time.time() - self.time_start
-               
-            #Calculate accuracy
-            count = 0
-            for i,c in enumerate(self.word):
-                try:
-                    if self.input_text[i] == c:
-                        count += 1
-                except:
-                    pass
-            self.accuracy = count/len(self.word)*100
-           
-            #Calculate words per minute
-            self.wpm = len(self.input_text)*60/(5*self.total_time)
-            self.end = True
-            print(self.total_time)
-                
-            self.results = 'Time:'+str(round(self.total_time)) +" secs   Accuracy:"+ str(round(self.accuracy)) + "%" + '   Wpm: ' + str(round(self.wpm))
-
-            # draw icon image
-            self.time_img = pygame.image.load('icon.png')
-            self.time_img = pygame.transform.scale(self.time_img, (150,150))
-            #screen.blit(self.time_img, (80,320))
-            screen.blit(self.time_img, (self.w/2-75,self.h-140))
-            self.draw_text(screen,"Reset", self.h - 70, 26, (100,100,100))
-            
-            print(self.results)
-            pygame.display.update()
-
-    def run(self):
-        self.reset_game()
-    
-       
-        self.running=True
-        while(self.running):
-            clock = pygame.time.Clock()
-            self.screen.fill((0,0,0), (50,250,650,50))
-            pygame.draw.rect(self.screen,self.HEAD_C, (50,250,650,50), 2)
-            # update the text of user input
-            self.draw_text(self.screen, self.input_text, 274, 26,(250,250,250))
-            pygame.display.update()
-            for event in pygame.event.get():
-                if event.type == QUIT:
-                    self.running = False
-                    sys.exit()
-                elif event.type == pygame.MOUSEBUTTONUP:
-                    x,y = pygame.mouse.get_pos()
-                    # position of input box
-                    if(x>=50 and x<=650 and y>=250 and y<=300):
-                        self.active = True
-                        self.input_text = ''
-                        self.time_start = time.time() 
-                     # position of reset box
-                    if(x>=310 and x<=510 and y>=390 and self.end):
-                        self.reset_game()
-                        x,y = pygame.mouse.get_pos()
-         
-                        
-                elif event.type == pygame.KEYDOWN:
-                    if self.active and not self.end:
-                        if event.key == pygame.K_RETURN:
-                            print(self.input_text)
-                            self.show_results(self.screen)
-                            print(self.results)
-                            self.draw_text(self.screen, self.results,350, 28, self.RESULT_C)  
-                            self.end = True
-                            
-                        elif event.key == pygame.K_BACKSPACE:
-                            self.input_text = self.input_text[:-1]
-                        else:
-                            try:
-                                self.input_text += event.unicode
-                            except:
-                                pass
-            
-            pygame.display.update()
-             
-                
-        clock.tick(60)
-
-    def reset_game(self):
-        self.screen.blit(self.open_img, (0,0))
-
-        pygame.display.update()
-        time.sleep(1)
-        
-        self.reset=False
-        self.end = False
-
-        self.input_text=''
-        self.word = ''
-        # self.time_start = 0
-        self.total_time = 0
-        self.wpm = 0
-
-        # Get random sentence 
-        self.word = self.get_sentence()
-        if (not self.word): self.reset_game()
-        #drawing heading
-        self.screen.fill((0,0,0))
-        self.screen.blit(self.bg,(0,0))
-        msg = "Typing Speed Test"
-        self.draw_text(self.screen, msg,80, 80,self.HEAD_C)  
-        # draw the rectangle for input box
-        pygame.draw.rect(self.screen,(255,192,25), (50,250,650,50), 2)
-
-        # draw the sentence string
-        self.draw_text(self.screen, self.word,200, 28,self.TEXT_C)
-        
-        pygame.display.update()
+# Function to draw
+def draw_floor():
+    screen.blit(floor_img, (floor_x, 520))
+    screen.blit(floor_img, (floor_x + 448, 520))
 
 
+# Function to create pipes
+def create_pipes():
+    pipe_y = random.choice(pipe_height)
+    top_pipe = pipe_img.get_rect(midbottom=(467, pipe_y - 300))
+    bottom_pipe = pipe_img.get_rect(midtop=(467, pipe_y))
+    return top_pipe, bottom_pipe
 
-Game().run()
+
+# Function for animation
+def pipe_animation():
+    global game_over, score_time
+    for pipe in pipes:
+        if pipe.top < 0:
+            flipped_pipe = pygame.transform.flip(pipe_img, False, True)
+            screen.blit(flipped_pipe, pipe)
+        else:
+            screen.blit(pipe_img, pipe)
+
+        pipe.centerx -= 3
+        if pipe.right < 0:
+            pipes.remove(pipe)
+
+        if bird_rect.colliderect(pipe):
+            game_over = True
+
+
+# Function to draw score
+def draw_score(game_state):
+    if game_state == "game_on":
+        score_text = score_font.render(str(score), True, (255, 255, 255))
+        score_rect = score_text.get_rect(center=(width // 2, 66))
+        screen.blit(score_text, score_rect)
+    elif game_state == "game_over":
+        score_text = score_font.render(f" Score: {score}", True, (255, 255, 255))
+        score_rect = score_text.get_rect(center=(width // 2, 66))
+        screen.blit(score_text, score_rect)
+
+        high_score_text = score_font.render(f"High Score: {high_score}", True, (255, 255, 255))
+        high_score_rect = high_score_text.get_rect(center=(width // 2, 506))
+        screen.blit(high_score_text, high_score_rect)
+
+
+# Function to update the score
+def score_update():
+    global score, score_time, high_score
+    if pipes:
+        for pipe in pipes:
+            if 65 < pipe.centerx < 69 and score_time:
+                score += 1
+                score_time = False
+            if pipe.left <= 0:
+                score_time = True
+
+    if score > high_score:
+        high_score = score
+
+
+# Game window
+width, height = 350, 622
+clock = pygame.time.Clock()
+screen = pygame.display.set_mode((width, height))
+pygame.display.set_caption("Flappy Bird")
+
+# setting background and base image
+back_img = pygame.image.load("img_46.png")
+floor_img = pygame.image.load("img_50.png")
+floor_x = 0
+
+# different stages of bird
+bird_up = pygame.image.load("img_47.png")
+bird_down = pygame.image.load("img_48.png")
+bird_mid = pygame.image.load("img_49.png")
+birds = [bird_up, bird_mid, bird_down]
+bird_index = 0
+bird_flap = pygame.USEREVENT
+pygame.time.set_timer(bird_flap, 200)
+bird_img = birds[bird_index]
+bird_rect = bird_img.get_rect(center=(67, 622 // 2))
+bird_movement = 0
+gravity = 0.17
+
+# Loading pipe image
+pipe_img = pygame.image.load("greenpipe.png")
+pipe_height = [400, 350, 533, 490]
+
+# for the pipes to appear
+pipes = []
+create_pipe = pygame.USEREVENT + 1
+pygame.time.set_timer(create_pipe, 1200)
+
+# Displaying game over image
+game_over = False
+over_img = pygame.image.load("img_45.png").convert_alpha ()
+over_rect = over_img.get_rect(center=(width // 2, height // 2))
+
+# setting variables and font for score
+score = 0
+high_score = 0
+score_time = True
+score_font = pygame.font.Font("freesansbold.ttf", 27)
+
+# Game loop
+running = True
+while running:
+    clock.tick(120)
+
+    # for checking the events
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:  # QUIT event
+            running = False
+            sys.exit()
+
+        if event.type == pygame.KEYDOWN:  # Key pressed event
+            if event.key == pygame.K_SPACE and not game_over:  # If space key is pressed
+                bird_movement = 0
+                bird_movement = -7
+
+            if event.key == pygame.K_SPACE and game_over:
+                game_over = False
+                pipes = []
+                bird_movement = 0
+                bird_rect = bird_img.get_rect(center=(67, 622 // 2))
+                score_time = True
+                score = 0
+
+        # To load different stages
+        if event.type == bird_flap:
+            bird_index += 1
+
+            if bird_index > 2:
+                bird_index = 0
+
+            bird_img = birds[bird_index]
+            bird_rect = bird_up.get_rect(center=bird_rect.center)
+
+        # To add pipes in the list
+        if event.type == create_pipe:
+            pipes.extend(create_pipes())
+
+    screen.blit(floor_img, (floor_x, 550))
+    screen.blit(back_img, (0, 0))
+
+    # Game over conditions
+    if not game_over:
+        bird_movement += gravity
+        bird_rect.centery += bird_movement
+        rotated_bird = pygame.transform.rotozoom(bird_img, bird_movement * -6, 1)
+
+        if bird_rect.top < 5 or bird_rect.bottom >= 550:
+            game_over = True
+
+        screen.blit(rotated_bird, bird_rect)
+        pipe_animation()
+        score_update()
+        draw_score("game_on")
+    elif game_over:
+        screen.blit(over_img, over_rect)
+        draw_score("game_over")
+
+    # To move the base
+    floor_x -= 1
+    if floor_x < -448:
+        floor_x = 0
+
+    draw_floor()
+
+    # Update the game window
+    pygame.display.update()
+
+# quiting the pygame and sys
+pygame.quit()
+sys.exit()
